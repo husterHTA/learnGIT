bab82c275b5fc21a6b8f40b5db6b90af3c1c05fa
husterHTA <anhhoang0083@gmail.com>
2021-12-15 09:30:55 +0700

    add file myProfile.java to demo

---
+++ src/demo/myProfile.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class myProfile {
+    public static void main(String[] args) {
+        System.out.println("My name is Anh");
+        System.out.println("I'm 25 years old");
+    }
+}
+
