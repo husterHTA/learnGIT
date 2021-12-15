cd42c423391ad0f1f5d8cd31ca7ea9abffb19ebe
husterHTA <anhhoang0083@gmail.com>
2021-12-15 09:34:21 +0700

    delete file myProfile.java

--- src/demo/myProfile.java
+++
@@ -1,9 +0,0 @@
-package demo;
-
-public class myProfile {
-    public static void main(String[] args) {
-        System.out.println("My name is Anh");
-        System.out.println("I'm 25 years old");
-    }
-}
-
